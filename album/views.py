7aec52511e4e6fac7f14308f23984c3ee5088cf1
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-13 01:33:24 +0900

    Hello World

--- album/views.py
+++ album/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse("<h1>Hello World!</h1>")
