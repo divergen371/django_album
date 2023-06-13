7e92ad22188d48df3f5547d2bdd82c5da2f17369
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-13 21:57:03 +0900

    :gear: 管理者にテーブルを表示させる

--- album/admin.py
+++ album/admin.py
@@ -1,3 +1,11 @@
+# Third Party Library
 from django.contrib import admin
 
+# Local Library
+from .models import Photo, Tag
+
 # Register your models here.
+
+
+admin.site.register(Tag)
+admin.site.register(Photo)
