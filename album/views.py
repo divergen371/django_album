e65a705113e8ff0a66d01c5737f3057569f61949
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-16 00:54:54 +0900

    :sparkles: タグで画像を絞り込む機能/view実装

--- album/views.py
+++ album/views.py
@@ -1,8 +1,9 @@
 # Third Party Library
 from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404
 
 # Local Library
-from .models import Photo
+from .models import Photo, Tag
 
 # Create your views here.
 
@@ -17,3 +18,14 @@ class PhotoDetailView(DetailView):
     model = Photo
     template_name = "album/photo_detail.html"
     context_object_name = "photo"
+
+
+class TagPhotoListView(ListView):
+    model = Photo
+    template_name = "album/tag_photo.html"
+    context_object_name = "photos"
+
+    def get_queryset(self, **kwargs):
+        tag_name = self.kwargs["tag"]
+        tag = get_object_or_404(Tag, name=tag_name)
+        return super().get_queryset().filter(tags=tag)
