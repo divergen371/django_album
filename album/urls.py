a197d0e0ac15800f54f3e4627fe2f4525201bb45
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-16 01:03:59 +0900

    :sparkles: タグで画像を絞り込む機能/タグ絞り込みページへのルーティング

--- album/urls.py
+++ album/urls.py
@@ -4,11 +4,12 @@ from django.conf.urls.static import static
 from django.urls import path
 
 # Local Library
-from .views import PhotoListView, PhotoDetailView
+from .views import PhotoListView, PhotoDetailView, TagPhotoListView
 
 urlpatterns = [
     path("", PhotoListView.as_view(), name="photo-list"),
     path("photo/<int:pk>", PhotoDetailView.as_view(), name="photo-detail"),
+    path("tag/<str:tag>/", TagPhotoListView.as_view(), name="tag-photo-list"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
