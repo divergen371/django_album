fbabb61e661707c3d7f9c306e80534d8b3249054
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-15 17:29:40 +0900

    :sparkles: ルーティング設定/トップページにアクセスがあったらPhotoListを呼び出す

--- album/urls.py
+++ album/urls.py
@@ -1,14 +1,13 @@
 # Third Party Library
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import path
 
 # Local Library
-from . import views
+from .views import PhotoListView
 
 urlpatterns = [
-    path("", views.index),
+    path("", PhotoListView.as_view(), name="photo-list"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
