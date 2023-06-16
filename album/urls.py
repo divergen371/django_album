b763897cb44cb8c1801bfd22c1aa294ccc9c1b49
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-16 21:48:24 +0900

    :sparkles: 新規投稿ページへのルーティング追加

--- album/urls.py
+++ album/urls.py
@@ -4,12 +4,18 @@ from django.conf.urls.static import static
 from django.urls import path
 
 # Local Library
-from .views import PhotoListView, PhotoDetailView, TagPhotoListView
+from .views import (
+    PhotoListView,
+    PhotoDetailView,
+    TagPhotoListView,
+    PhotoCreateView,
+)
 
 urlpatterns = [
     path("", PhotoListView.as_view(), name="photo-list"),
     path("photo/<int:pk>", PhotoDetailView.as_view(), name="photo-detail"),
     path("tag/<str:tag>/", TagPhotoListView.as_view(), name="tag-photo-list"),
+    path("create/", PhotoCreateView.as_view(), name="photo-create"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
